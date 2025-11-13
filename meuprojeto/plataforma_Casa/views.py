5154836ee9267200b9f914afb695231b305376ff
BeCasanovas <becasanovas@gmail.com>
2025-11-13 20:54:09 -0300

    Corrigindo erro de importação no login

--- meuprojeto/plataforma_Casa/views.py
+++ meuprojeto/plataforma_Casa/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth import login
 from django.contrib import messages
 from .permission import (
             is_adm, 
